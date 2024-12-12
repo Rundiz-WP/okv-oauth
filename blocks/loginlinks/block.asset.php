68a69893c67628910cb89b9f7495f5566db99e7d
ve3 <kawewong@gmail.com>
2024-12-12 09:44:46 +0700

    Update Node package for security fix.
    This has no effect to users at all because it is for use with `wp-scripts` command when edit the plugin's block.

--- blocks/loginlinks/block.asset.php
+++ blocks/loginlinks/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '73bfb80712124e49bd37');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '59f9ebbb1eff8b6773b5');
