2f98fb29ec609f839e0a41ae8187465258629a1b
ve3 <kawewong@gmail.com>
2026-03-23 01:36:14 +0700

    Rebuild blocks.

--- blocks/loginlinks/block.asset.php
+++ blocks/loginlinks/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '59f9ebbb1eff8b6773b5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '47ac2a40f9012e17d20f');
--- blocks/loginlinks/render.php
+++ blocks/loginlinks/render.php
@@ -2,59 +2,76 @@
 /**
  * Render contents for loginlinks block.
  * 
- * @package rundiz-oauth
+ * @package okv-oauth
  * @since 1.5
  */
 
 
-$currentUrl = ( is_ssl() ? 'https://' : 'http://' ) . 
+if (!defined('ABSPATH')) {
+    exit();
+}
+
+$okv_oauth_currentUrl = ( is_ssl() ? 'https://' : 'http://' ) . 
     (isset($_SERVER['HTTP_HOST']) ? sanitize_text_field(wp_unslash($_SERVER['HTTP_HOST'])) : '') . 
     (isset($_SERVER['REQUEST_URI']) ? sanitize_text_field(wp_unslash($_SERVER['REQUEST_URI'])) : '');
-$isUserLoggedIn = is_user_logged_in();
-$classes = ($isUserLoggedIn ? 'logged-in' : 'logged-out');
-$wrapperAttributes = get_block_wrapper_attributes(['class' => $classes]);
-unset($classes);
-$openLine = '<li>';
-$closeLine = '</li>';
+$okv_oauth_isUserLoggedIn = is_user_logged_in();
+$okv_oauth_classes = ($okv_oauth_isUserLoggedIn ? 'logged-in' : 'logged-out');
+$okv_oauth_wrapperAttributes = get_block_wrapper_attributes(['class' => $okv_oauth_classes]);
+unset($okv_oauth_classes);
+$okv_oauth_openLine = '<li>';
+$okv_oauth_closeLine = '</li>';
 
-$contents = '';
-$contents .= '<ul ' . $wrapperAttributes . '>';
-unset($wrapperAttributes);
+$okv_oauth_contents = '';
+$okv_oauth_contents .= '<ul ' . $okv_oauth_wrapperAttributes . '>';
+unset($okv_oauth_wrapperAttributes);
 // apply filters after open the wrapper.
 // @since 1.5.2
-$contents .= apply_filters('rdoauth_loginlinkswidgetblock_afteropenwrapper', '', $openLine, $closeLine);
+// @since 1.7.0 Renamed from `rdoauth_loginlinkswidgetblock_afteropenwrapper`.
+$okv_oauth_contents .= apply_filters('okv_oauth_loginlinkswidgetblock_afteropenwrapper', '', $okv_oauth_openLine, $okv_oauth_closeLine);
 
-if (!$isUserLoggedIn && get_option('users_can_register')) {
+if (!$okv_oauth_isUserLoggedIn && get_option('users_can_register')) {
     // if user is not logged in and has option can register.
-    $contents .= $openLine . '<a href="' . esc_url( wp_registration_url() ) . '">' . __( 'Register' ) . '</a>' . $closeLine . PHP_EOL;
-} elseif ($isUserLoggedIn) {
+    $okv_oauth_contents .= $okv_oauth_openLine . '<a href="' . esc_url( wp_registration_url() ) . '">' . esc_html__('Register', 'okv-oauth') . '</a>' . $okv_oauth_closeLine . PHP_EOL;
+} elseif ($okv_oauth_isUserLoggedIn) {
     // if user logged in.
     // apply filters for logged in users, before display links.
     // @since 1.5.2
-    $contents .= apply_filters('rdoauth_loginlinkswidgetblock_loggedin_beforelinks', '', $openLine, $closeLine);
+    // @since 1.7.0 Renamed from `rdoauth_loginlinkswidgetblock_loggedin_beforelinks`.
+    $okv_oauth_contents .= apply_filters('okv_oauth_loginlinkswidgetblock_loggedin_beforelinks', '', $okv_oauth_openLine, $okv_oauth_closeLine);
     if (isset($attributes) && is_array($attributes) && array_key_exists('displayLinkToAdmin', $attributes) && true === $attributes['displayLinkToAdmin']) {
         // if block was set to display link to admin dashboard.
-        $contents .= $openLine . '<a href="' . admin_url() . '">' . __( 'Site Admin' ) . '</a>' . $closeLine . PHP_EOL;
+        $okv_oauth_contents .= $okv_oauth_openLine . '<a href="' . admin_url() . '">' . esc_html__('Site Admin', 'okv-oauth') . '</a>' . $okv_oauth_closeLine . PHP_EOL;
     }
     if (isset($attributes) && is_array($attributes) && array_key_exists('displayLinkToEditProfile', $attributes) && true === $attributes['displayLinkToEditProfile']) {
         // if block was set to display link to edit profile.
-        $contents .= $openLine . '<a href="' . get_edit_user_link() . '">' . __('Edit Profile') . '</a>' . $closeLine . PHP_EOL;
+        $okv_oauth_contents .= $okv_oauth_openLine . '<a href="' . get_edit_user_link() . '">' . esc_html__('Edit Profile', 'okv-oauth') . '</a>' . $okv_oauth_closeLine . PHP_EOL;
     }
     // apply filters for logged in users, after  display links.
     // @since 1.5.2
-    $contents .= apply_filters('rdoauth_loginlinkswidgetblock_loggedin_afterlinks', '', $openLine, $closeLine);
+    // @since 1.7.0 Renamed from `rdoauth_loginlinkswidgetblock_loggedin_afterlinks`.
+    $okv_oauth_contents .= apply_filters('okv_oauth_loginlinkswidgetblock_loggedin_afterlinks', '', $okv_oauth_openLine, $okv_oauth_closeLine);
 }
-unset($isUserLoggedIn);
+unset($okv_oauth_isUserLoggedIn);
 
-$contents .= $openLine . wp_loginout($currentUrl, false) . $closeLine . PHP_EOL;
+$okv_oauth_contents .= $okv_oauth_openLine . wp_loginout($okv_oauth_currentUrl, false) . $okv_oauth_closeLine . PHP_EOL;
 // apply filters after login/logout.
 // @since 1.5.2
-$contents .= apply_filters('rdoauth_loginlinkswidgetblock_afterloginout', '', $openLine, $closeLine);
-unset($currentUrl);
-$contents .= '</ul>';
+// @since 1.7.0 Renamed from `rdoauth_loginlinkswidgetblock_afterloginout`.
+$okv_oauth_contents .= apply_filters('okv_oauth_loginlinkswidgetblock_afterloginout', '', $okv_oauth_openLine, $okv_oauth_closeLine);
+unset($okv_oauth_currentUrl);
+$okv_oauth_contents .= '</ul>';
 
-unset($closeLine, $openLine);
+unset($okv_oauth_closeLine, $okv_oauth_openLine);
 
+$okv_oauth_kses_file = dirname(OKVOAUTH_FILE) . '/App/config/kses_data.php';
+if (!is_file($okv_oauth_kses_file)) {
+    // if not found custom kses data. This use custom kses data to make sure it is up to date with modern HTML elements and attributes that will work.
+    // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+    error_log(esc_html('The file ' . str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $okv_oauth_kses_file) . ' could not be found.'));
+}
 // must use echo, not return.
-echo $contents;
+// not use `require` because if kses data file is not exists, the page must still be able to show.
+// phpcs:ignore PEAR.Files.IncludingFile.UseRequire
+echo wp_kses($okv_oauth_contents, include $okv_oauth_kses_file);
+unset($okv_oauth_kses_file);
 // that's all. end of file.
