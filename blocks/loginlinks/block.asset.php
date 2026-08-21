2a32add512bb121db2e23959896a1fda09d0a428
ve3 <kawewong@gmail.com>
2026-08-21 23:57:51 +0700

    Rebuild blocks.

--- blocks/loginlinks/block.asset.php
+++ blocks/loginlinks/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '47ac2a40f9012e17d20f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e8133599264e624a66b5');
